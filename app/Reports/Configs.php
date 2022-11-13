5e0c8161bec62d4dbb4a8cd003d089a1830b8a23
goldenfay <s.faycal@ouedkniss.com>
2022-11-13 22:02:31 +0100

    [Fix] Set koolreport config

--- app/Reports/Configs.php
+++ app/Reports/Configs.php
@@ -3,7 +3,7 @@ namespace App\Reports;
 class Configs 
 {
     public static $PHP_MYADMIN_USERNAME="root";
-    public static $PHP_MYADMIN_PASSWORD="";
+    public static $PHP_MYADMIN_PASSWORD="Ifast2022$";
     public static $PHP_MYADMIN_DBNAME="projet_scolarite";
     
 }
