e04c182187aafd9b193d79a71b5a186f787a8191
goldenfay <s.faycal@ouedkniss.com>
2022-11-14 21:56:15 +0100

    [Fix] Use env vars as Koolreport config

--- app/Reports/Configs.php
+++ app/Reports/Configs.php
@@ -2,9 +2,9 @@
 namespace App\Reports;
 class Configs 
 {
-    public static $PHP_MYADMIN_USERNAME="root";
-    public static $PHP_MYADMIN_PASSWORD="Ifast2022$";
-    public static $PHP_MYADMIN_DBNAME="projet_scolarite";
+    public static $PHP_MYADMIN_USERNAME=env("DB_USERNAME","root");
+    public static $PHP_MYADMIN_PASSWORD=env("DB_USERNAME","");
+    public static $PHP_MYADMIN_DBNAME=env("DB_DATABASE","projet_scolarite");
     
 }
 ?>
