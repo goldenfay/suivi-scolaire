ba232419aedf8c84ae7969d25dbbcdf4c8e9b234
goldenfay <s.faycal@ouedkniss.com>
2022-11-23 19:03:20 +0100

    [Feature]  Improve sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,46 +1,45 @@
-<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
+<div class="sidebar" data-color="orange" data-background-color="white"
+    data-image="{{ asset('material') }}/img/sidebar-1.jpg">
 
-  <div class="logo my-5">
-    <a href="#" class="simple-text logo-normal">
-      khkgjhghjkgkjghg
-      {{-- {{ __('Bonjour') }} --}}
-      @if(Auth::check())
-      
-      {{Auth::user()->Nom}} {{Auth::user()->Prenom}}
-      @elseif(Auth::guard('admin')->check())
-      {{ __('Bonjour Administrateur') }}
+    <div class="logo my-5">
+        <a href="#" class="simple-text logo-normal">
+            {{-- {{ __('Bonjour') }} --}}
+            @if (Auth::check())
 
-      @else
-      {{ __('Bonjour') }}
-      @endif
-    </a>
-  </div>
-  <div class="sidebar-wrapper mt-5">
-    @if(Auth::guard('prof')->check())
-      @include('layouts.navbars.sidebars.prof-sidebar')
-    @elseif(Auth::guard('admin')->check())
-      @include('layouts.navbars.sidebars.admin-sidebar')
-    @elseif(Auth::check())
-      @include('layouts.navbars.sidebars.parent-sidebar')
-    @endif
-    
-    <div class="mt-5 ps-container ps-theme-default">
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link" 
-          href="{{ route('logout') }}"
-          onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-          >
-            <i class="material-icons">power_settings_new</i>
-            <p>{{ __('Déconnexion') }}</p>
-          </a>
-        </li>
-  
-      </ul>
-      
+                @if (Auth::guard('admin')->check())
+                    {{ __('Bonjour Administrateur') }}
+                @else
+                    {{ Auth::user()->Nom }} {{ Auth::user()->Prenom }}
+                @endif
+            @else
+                {{ __('Bonjour') }}
+            @endif
+        </a>
     </div>
+    <div class="sidebar-wrapper mt-5">
+        @if (Auth::guard('prof')->check())
+            @include('layouts.navbars.sidebars.prof-sidebar')
+        @elseif(Auth::guard('admin')->check())
+            @include('layouts.navbars.sidebars.admin-sidebar')
+        @elseif(Auth::check())
+            @include('layouts.navbars.sidebars.parent-sidebar')
+        @endif
+
+        <div class="mt-5 ps-container ps-theme-default">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        <i class="material-icons">power_settings_new</i>
+                        <p>{{ __('Déconnexion') }}</p>
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
 
 
 
-  </div>
+    </div>
 </div>
